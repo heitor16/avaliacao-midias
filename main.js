71991424bca85f71ad4d17b135c485f0910dd2a1
heitor13pt <126159387+heitor16@users.noreply.github.com>
2023-11-10 10:49:16 -0300

    s

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocasom(idElemntoAudio){
+    document.querySelector(idElemntoAudio).play();
+}
+const listadeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < listadeTeclas.length){
+const efeito = listadeTeclas[contador].classList[1];
+const idAudio = "#som_"+efeito;
+listadeTeclas[contador].onclick = finction(){
+    tocasom (idAudio)
+}
+    contador = contador +1;
+    console.log(contador);
+}
