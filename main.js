c87093c2b551223e2dfc778356d6c22f607bd9c3
heitor13pt <126159387+heitor16@users.noreply.github.com>
2023-11-10 10:50:14 -0300

    s

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let contador = 0;
 while(contador < listadeTeclas.length){
 const efeito = listadeTeclas[contador].classList[1];
 const idAudio = "#som_"+efeito;
-listadeTeclas[contador].onclick = finction(){
+listadeTeclas[contador].onclick = function(){
     tocasom (idAudio)
 }
     contador = contador +1;
